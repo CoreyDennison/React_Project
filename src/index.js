b0cffca3a872e9af8fe1d8be121ca078610bdb3f
Corey Dennison <cmgdennison@gmail.com>
2019-03-03 18:13:01 +0000

    Created search bar and button
    Having issues with uploading images through React

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './reset.css';
 import './box.css';
+import TMDB_Logo from './TMDB_Logo.png';
+import blank from './blank.jpg'
 
-class Header_Footer extends React.Component {
+class HeaderFooter extends React.Component {
     render() {
       return (
         <div className = 'header'>
@@ -17,16 +19,33 @@ class Header_Footer extends React.Component {
     render() {
       return (
         <div className = 'content'>
+          <input type='text' name='search' />
+          <button>Search</button>
         </div>
       )
     }
   }
-   
+
+  class MovieImgs extends React.Component {
+    render() {
+      return (
+        <div className = 'movieimg'> 
+          <img src={blank} alt='Blank'/>
+        </div>
+      )
+    }
+  }
+
   ReactDOM.render(
     <div>
-      <Header_Footer />
-      <Content />
-      <Header_Footer />
-      </div>,
+      <HeaderFooter />
+      <Content>
+        <MovieImgs />
+      </Content>
+      <HeaderFooter>
+        <img src={TMDB_Logo} alt='Logo' />
+      </HeaderFooter>
+    </div>,
+
     document.getElementById('root')
   );
