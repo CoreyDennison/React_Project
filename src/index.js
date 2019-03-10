9700b5a6a0885c9f0c163d2154b4f6aebe08e0e2
Corey Dennison <cmgdennison@gmail.com>
2019-03-10 18:07:11 +0000

    Fetched data in console
    Have desired elements appearing in console using axios to fetch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './reset.css';
 import './box.css';
+import axios from 'axios'
 
 // https://api.themoviedb.org/3/search/movie?api_key=ec4d5ba75bc6742169af2ca01826ade6&language=en-US&query=${blade}&page=1&include_adult=false
 // fetch(url).then(response => response.json()).then(data => setState)
@@ -21,36 +22,37 @@ class Content extends React.Component {
   constructor(props) {
     super(props)
 
-      const movies = [
-        {id: 0, poster_src: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",  title: "Avengers, Infinity Wars", release: 2016, genre: "action"}
-      ]
-
-      var movieRows = [];
-
-      movies.forEach((movie) => {
-        const movieRow = <ul key={movie.id}>
-                <li><img width="180" height="200" alt="poster" src={movie.poster_src} /></li>
-                <li>{movie.title}</li>
-                <li>{movie.release}</li>
-                <li>{movie.genre}</li>
-              </ul>
-        movieRows.push(movieRow)
-      })
-
-      this.state = {rows: movieRows}
-
+      this.doSearch()
   }
 
+  doSearch() {
+
+    const urlString ="https://api.themoviedb.org/3/search/movie?api_key=ec4d5ba75bc6742169af2ca01826ade6&language=en-US&query=blade&page=1&include_adult=false"
+    
+    axios.get(urlString)
+      .then(res => {
+
+        //shows top 10 results
+        const movieResults = res.data.results.slice(0,10)
+
+        console.log(movieResults)
+
+        var movieRows = [ ]
+
+        movieResults.forEach((movie) => {
+          console.log(movie.title, movie.release_date.slice(0,4), movie.vote_average)
+        })
+
+        })
+    }
+  
   render() {
     return (
       <div className = 'content'>
         <input type='text' name='search' />
-        <button>Search</button>
+        <button >Search</button>
 
-        {this.state.rows}
-
-      </div>
-         
+      </div>    
     )
   }
 }
