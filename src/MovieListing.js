8cdbb13638069133cc8f0bfa5e5e557fb4dc9c79
Corey Dennison <cmgdennison@gmail.com>
2019-03-12 21:09:24 +0000

    Made rows and search
    Data is displayed in rows and beneficial search function has been implemented

--- src/MovieListing.js
+++ src/MovieListing.js
@@ -3,7 +3,7 @@ import React from 'react'
 class MovieListing extends React.Component {
     render() {
         return <ul key={this.props.movie.id}>
-        <li><img alt="poster" src={this.props.movie.poster_src} /></li>
+        <li><img alt="poster" src={this.props.movie.poster_src} width="180px" length="200px" /></li>
         <li>{this.props.movie.title}</li>
         <li>Released: {this.props.movie.release_date.slice(0,4)}</li>
         <li>Rating: {this.props.movie.vote_average}</li>
--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import axios from 'axios'
 class Header extends React.Component {
     render() {
       return (
-        <div className = 'header'>
+        <div className = 'headerfooter'>
+          <h1>Movie Search</h1>
         </div>
       )
     }
@@ -23,65 +24,47 @@ class Content extends React.Component {
   constructor(props){
     super(props)
 
-    this.state = {}
-
-    //console.log("init")
-
-    /*var movies = [
-      {id:0, poster_src: "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg" , title: "Blade", year: 2002, rate: 6.3}
-    ]
-
-    this.state = {films: <ul>
-      <li>Title</li>
-      <li>Year</li>
-      <li>Rating</li>
-    </ul>
-    }
-
-    var movieListings = []
-
-    movies.forEach((movie) => {
-      console.log(movie.title)
-      var movieListing = <MovieListing movie={movie}/>
-      movieListings.push(movieListing)
-    })
-    this.state = {films: movieListings}*/
+    this.state = {films: null, searchValue: ''};
 
     this.doSearch()
-    console.log("Do Search")
+    //Strictly binds "this" to "Content" class
+    this.doSearch = this.doSearch.bind(this);
+    this.updateInput = this.updateInput.bind(this);
   }
 
-  doSearch(){
-    const tmdb_url = "https://api.themoviedb.org/3/search/movie?api_key=ec4d5ba75bc6742169af2ca01826ade6&language=en-US&query=blade runner"
-    axios.get(tmdb_url).then(res => {
+  async doSearch(){
+    const movieName = this.state.searchValue;
+    const tmdb_url = `https://api.themoviedb.org/3/search/movie?api_key=ec4d5ba75bc6742169af2ca01826ade6&language=en-US&query=${movieName}`
+    const res = await axios.get(tmdb_url);
     //shows top 10 results
-    const movieResults = res.data.results.slice(0,2)
+    const movieResults = res.data.results.slice(0,10)
 
-    console.log(movieResults)
-
-    var movieListings = []
-    //50 minutes into video
     movieResults.forEach((movie) => {
       var movieYear = movie.release_date.slice(0,4)
       movie.poster_src = "http://image.tmdb.org/t/p/w185" + movie.poster_path
-
-      console.log(movie.poster_src,movie.title, movieYear, movie.vote_average)
-      var movieListing = <MovieListing movie={movie} />
-      movieListings.push(movieListing)
     })
-    this.setState({films: movieListings})
-  })
 
-  }
+    this.setState({films: movieResults})
   
+  }
+
+  updateInput (event) {
+    this.setState({searchValue: event.target.value})
+    
+  }
 
   render() {
     return (
       <div className = 'content'>
-        <input type='text' name='search' />
-        <button >Search</button>
+        <input type='text' name='search' value={this.state.searchValue} onChange={this.updateInput} onInput={this.updateInput}/>
+        <button id="searchButton" onClick={this.doSearch} >Search</button>
 
-        {this.state.films}
+        <div className='row'>
+        {
+          this.state.films && 
+            this.state.films.map(film => <MovieListing key={film.id} movie={film} />) 
+        }
+        </div>
 
       </div>    
     )
@@ -91,7 +74,7 @@ class Content extends React.Component {
 class Footer extends React.Component {
   render() {
     return (
-      <div className = 'footer'>
+      <div className = 'headerfooter'>
         <div className = 'logo' />
       </div>
     )
